bdef94ff735a6f0b45b5889c980f1eabc5bff235
Anton Zenkov <azenkov@lincolnlabs.co>
2022-07-01 12:46:32 -0500

    test

--- src/main/kotlin/IpUtil.kt
+++ src/main/kotlin/IpUtil.kt
@@ -1,5 +1,4 @@
 import java.io.File
-import java.net.Inet4Address
 import java.util.*
 
 
@@ -10,30 +9,49 @@ class IpUtil {
             var duplicateIps = 0
             var unique = 0
 
+            val ips = mutableMapOf<Int, Map<Int, Map<Int, Int>>>()
+            val ips2 = mutableMapOf<Int, Map<Int, Int>>()
+            val ips3 = mutableMapOf<Int, Int>()
+
             file.bufferedReader().forEachLine {
-                var ipInt = ipToInt(it)
-                if(ipInt < 0) {
-                    ipInt *= -1
+                val ipInt = ipToInt(it)
+                var skip = false
+                
+                if(ips3[ipInt[2]] == ipInt[3]) {
+                    when {
+                        ips2[ipInt[1]] == ips3 &&
+                        ips[ipInt[0]] == ips2 -> {
+                            duplicateIps++
+                            skip = true
+                        }
+                    }
                 }
 
-                if (bitIps[ipInt]) {
-                    duplicateIps++
+                if(!skip) {
+                    ips3[ipInt[2]] = ipInt[3]
+                    ips2[ipInt[1]] = ips3
+                    ips[ipInt[0]] = ips2
                 }
-                bitIps[ipInt] = true
+                
                 unique++
             }
+            
+//            println(ips)
 
             return Pair(unique - duplicateIps, duplicateIps)
         }
+        
+        
 
-        private fun ipToInt(ip: String): Int {
-            val ipAddress = Inet4Address.getByName(ip)
-            var result = 0
-            for (b in ipAddress.address) {
-                result = result shl 8 or (b.toInt() and 0xFF)
+        private fun ipToInt(ipString: String): IntArray {
+            val ip = IntArray(4)
+            val parts = ipString.split("\\.".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+
+            for (i in 0..3) {
+                ip[i] = parts[i].toInt()
             }
-
-            return result
+            
+            return ip
         }
     }
 }
